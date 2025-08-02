efec87da58cf5a64959b6edc1b2aaaa2488007fd
shimronduan <shimron_duan@next.co.uk>
2025-08-02 01:14:25 +0100

    Specify version constraint for azurerm provider in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~>3.90"
     }
   }
   # You should set up a new, unique state file key for this project
