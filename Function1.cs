2e676ab4865841536122044dc23f11820982c635
shimronduan <shimron_duan@next.co.uk>
2025-08-02 15:04:58 +0100

    Refactor Function1 to read request body and return MultiResponse; add Azure Storage Queue extension package

--- Function1.cs
+++ Function1.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -5,20 +7,38 @@ using Microsoft.Extensions.Logging;
 
 namespace Tradingbot.Mediator;
 
-public class Function1
+public class Function1(ILogger<Function1> logger)
 {
-    private readonly ILogger<Function1> _logger;
-
-    public Function1(ILogger<Function1> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<Function1> _logger = logger;
 
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<MultiResponse> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+        return new MultiResponse()
+        {
+            Message = new MyQueueItem
+            {
+                Body = requestBody
+            },
+            HttpResponse = new OkObjectResult("Request body sent to queue.")
+        };
     }
+}
+
+public class MultiResponse
+{
+    [QueueOutput("futures", Connection = "QueueConnection")]
+    public MyQueueItem? Message { get; set; }
+
+    [HttpResult]
+    public IActionResult? HttpResponse { get; set; }
+}
+
+public class MyQueueItem
+{
+    public string? Body { get; set; }
 }
